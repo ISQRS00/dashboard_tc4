74637e8f4aaf1ad0cfa380d4678045e69d612afa
Isaque Ramalho Dos Santos <168669233+ISQRS00@users.noreply.github.com>
2024-12-03 07:23:18 -0300

    Update Previsões_ETS.py

--- pages/Previsões_ETS.py
+++ pages/Previsões_ETS.py
@@ -5,7 +5,6 @@ import plotly.graph_objects as go
 from datetime import timedelta
 import statsmodels.api as sm
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-import os
 
 # Configurações do Streamlit
 st.set_page_config(page_title="Deploy | Tech Challenge 4 | FIAP", layout='wide')
@@ -15,7 +14,7 @@ st.set_page_config(page_title="Deploy | Tech Challenge 4 | FIAP", layout='wide')
 def wmape(y_true, y_pred):
     return np.abs(y_true - y_pred).sum() / np.abs(y_true).sum()
 
-# Carregar e preparar os dados (com cache)
+# Carregar e preparar os dados
 @st.cache_data
 def load_data():
     df = pd.read_csv('https://raw.githubusercontent.com/ISQRS00/dashboard_tc4/main/barril.csv', sep=';')
@@ -26,7 +25,7 @@ def load_data():
     df['realizado'] = df['realizado'].ffill()  # Preencher valores ausentes
     return df
 
-# Função para treinar o modelo ETS (utilizar cache de recursos)
+# Função para treinar o modelo ETS
 @st.cache_resource
 def train_ets_model(train_data, season_length=252):
     model_ets = sm.tsa.ExponentialSmoothing(train_data['realizado'], seasonal='mul', seasonal_periods=season_length).fit()
@@ -34,7 +33,7 @@ def train_ets_model(train_data, season_length=252):
 
 # Função para previsão com o modelo ETS
 @st.cache_data
-def forecast_ets(train, valid, _model_ets):  # Modificado para _model_ets
+def forecast_ets(train, valid, _model_ets):
     forecast_ets = _model_ets.forecast(len(valid))
     forecast_dates = pd.date_range(start=train['data'].iloc[-1] + pd.Timedelta(days=1), periods=len(valid), freq='D')
     ets_df = pd.DataFrame({'data': forecast_dates, 'previsão': forecast_ets})
@@ -58,8 +57,8 @@ O modelo de previsão ETS é treinado com base em dados históricos do preço do
 Escolha o número de dias para o corte e veja como o modelo se comporta para diferentes períodos.
 """)
 
-# Input para o número de dias para corte
-dias_corte = st.number_input('Selecione o número de dias para o corte entre 7 e 90:', min_value=7, max_value=90, value=7)
+# Caixa de seleção para escolher o número de dias para corte
+dias_corte = st.selectbox('Selecione o número de dias para o corte:', options=[7, 12, 30], index=1)
 
 # Calcular a data de corte com base no número de dias
 cut_date = df_barril_petroleo['data'].max() - timedelta(days=dias_corte)
@@ -73,47 +72,39 @@ st.write(f"A data de corte é: {cut_date}")
 st.write(f"Tamanho do conjunto de treino: {len(train)}")
 st.write(f"Tamanho do conjunto de validação: {len(valid)}")
 
-# Criar uma barra de progresso
-progress = st.progress(0)
-
 # Treinar o modelo ETS
-if st.button("Gerar Previsão"):
-    # Atualizar barra de progresso para indicar que o treinamento está em andamento
-    for i in range(100):
-        progress.progress(i + 1)
-    
-    # Treinar o modelo
-    model_ets = train_ets_model(train)
-    
-    # Prever com o modelo ETS
-    ets_df, wmape_ets, MAE_ets, MSE_ets, R2_ets = forecast_ets(train, valid, model_ets)
+model_ets = train_ets_model(train)
 
-    st.subheader('Métricas de Desempenho do Modelo ETS')
-    st.write(f'WMAPE: {wmape_ets:.2%}')
-    st.write(f'MAE: {MAE_ets:.3f}')
-    st.write(f'MSE: {MSE_ets:.4f}')
-    st.write(f'R²: {R2_ets:.2f}')
+# Prever com o modelo ETS
+ets_df, wmape_ets, MAE_ets, MSE_ets, R2_ets = forecast_ets(train, valid, model_ets)
 
-    # Criar gráfico ETS
-    fig_ets = go.Figure()
-    fig_ets.add_trace(go.Scatter(x=valid['data'], y=valid['realizado'], mode='lines', name='Realizado'))
-    fig_ets.add_trace(go.Scatter(x=ets_df['data'], y=ets_df['previsão'], mode='lines', name='Forecast'))
-    fig_ets.update_layout(
-        title="Previsão do Modelo ETS",
-        xaxis_title="Data",
-        yaxis_title="Valor do Petróleo (US$)",
-        xaxis=dict(tickformat="%d-%m-%Y", tickangle=45),
-        yaxis=dict(title="Valor (US$)", tickformat=".3f"),
-        autosize=True
-    )
-    st.plotly_chart(fig_ets, use_container_width=True)
+# Exibir métricas de desempenho
+st.subheader('Métricas de Desempenho do Modelo ETS')
+st.write(f'WMAPE: {wmape_ets:.2%}')
+st.write(f'MAE: {MAE_ets:.3f}')
+st.write(f'MSE: {MSE_ets:.4f}')
+st.write(f'R²: {R2_ets:.2f}')
 
-    # Opção de Download dos Resultados
-    st.subheader('Baixar Resultados')
-    csv = ets_df.to_csv(index=False)
-    st.download_button(
-        label="Baixar Previsões ETS",
-        data=csv,
-        file_name="previsoes_ets.csv",
-        mime="text/csv"
-    )
+# Criar gráfico ETS
+fig_ets = go.Figure()
+fig_ets.add_trace(go.Scatter(x=valid['data'], y=valid['realizado'], mode='lines', name='Realizado'))
+fig_ets.add_trace(go.Scatter(x=ets_df['data'], y=ets_df['previsão'], mode='lines', name='Forecast'))
+fig_ets.update_layout(
+    title="Previsão do Modelo ETS",
+    xaxis_title="Data",
+    yaxis_title="Valor do Petróleo (US$)",
+    xaxis=dict(tickformat="%d-%m-%Y", tickangle=45),
+    yaxis=dict(title="Valor (US$)", tickformat=".3f"),
+    autosize=True
+)
+st.plotly_chart(fig_ets, use_container_width=True)
+
+# Opção de Download dos Resultados
+st.subheader('Baixar Resultados')
+csv = ets_df.to_csv(index=False)
+st.download_button(
+    label="Baixar Previsões ETS",
+    data=csv,
+    file_name="previsoes_ets.csv",
+    mime="text/csv"
+)
