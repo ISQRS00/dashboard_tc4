66bde7a4fc0fde218ea6110eaac915bdbc211daa
Isaque Ramalho Dos Santos <168669233+ISQRS00@users.noreply.github.com>
2024-12-03 08:03:51 -0300

    Update Previsões_ETS.py

--- pages/Previsões_ETS.py
+++ pages/Previsões_ETS.py
@@ -4,28 +4,23 @@ import plotly.graph_objects as go
 import statsmodels.api as sm
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from datetime import timedelta
-import pandas as pd
 import plotly.express as px
 import os
-import streamlit as st
-import pandas as pd
 import numpy as np
-import plotly.graph_objects as go
-from datetime import date, timedelta
-from sklearn.metrics import accuracy_score, r2_score, mean_absolute_error, mean_squared_error
-import statsmodels.api as sm
 from statsmodels.tsa.seasonal import seasonal_decompose
 import yfinance as yf
 from statsforecast import StatsForecast
 from plotly.subplots import make_subplots
+
 # Função para calcular WMAPE
 def wmape(y_true, y_pred):
     return np.abs(y_true - y_pred).sum() / np.abs(y_true).sum()
 
-# Função para treinar o modelo ETS
+# Função para treinar o modelo ETS com cache
+@st.cache_data
 def train_ets_model(train_data):
     season_length = 252  # Sazonalidade anual
-    model_ets = sm.tsa.ExponentialSmoothing(train_data['realizado'], seasonal='mul', seasonal_periods=season_length).fit()
+    model_ets = sm.tsa.ExponentialSmoothing(train_data['realizado'], seasonal='mul', seasonal_periods=season_length).fit(optimized=True)
     return model_ets
 
 # Configurações do Streamlit
@@ -37,7 +32,7 @@ def load_data():
     df = pd.read_csv('https://raw.githubusercontent.com/ISQRS00/dashboard_tc4/main/barril.csv', sep=';')
     df.drop(columns=['Unnamed: 2'], inplace=True)
     df.rename(columns={'Data': 'data', 'Preço - petróleo bruto - Brent (FOB) - US$ - Energy Information Administration (EIA) - EIA366_PBRENT366': 'realizado'}, inplace=True)
-    df['data'] = pd.to_datetime(df['data'])
+    df['data'] = pd.to_datetime(df['data'], format='%d/%m/%Y')
     df['realizado'] = df['realizado'].str.replace(',', '.').astype(float)
     df['realizado'] = df['realizado'].ffill()  # Preencher valores ausentes
     return df
@@ -68,19 +63,21 @@ train = df_barril_petroleo.loc[df_barril_petroleo['data'] < cut_date]
 valid = df_barril_petroleo.loc[df_barril_petroleo['data'] >= cut_date]
 
 # Função para previsão com o modelo ETS
+@st.cache_data
 def forecast_ets(train, valid):
-    season_length = 252  # Sazonalidade anual
-    model_ets = sm.tsa.ExponentialSmoothing(train['realizado'], seasonal='mul', seasonal_periods=season_length).fit()
-    forecast_ets = model_ets.forecast(len(valid))
-    forecast_dates = pd.date_range(start=train['data'].iloc[-1] + pd.Timedelta(days=1), periods=len(valid), freq='D')
-    ets_df = pd.DataFrame({'data': forecast_dates, 'previsão': forecast_ets})
-    ets_df = ets_df.merge(valid, on=['data'], how='inner')
-    
-    # Calcular métricas de desempenho
-    wmape_ets = wmape(ets_df['realizado'].values, ets_df['previsão'].values)
-    MAE_ets = mean_absolute_error(ets_df['realizado'].values, ets_df['previsão'].values)
-    MSE_ets = mean_squared_error(ets_df['realizado'].values, ets_df['previsão'].values)
-    R2_ets = r2_score(ets_df['realizado'].values, ets_df['previsão'].values)
+    with st.spinner('Treinando o modelo e gerando previsões...'):
+        season_length = 252  # Sazonalidade anual
+        model_ets = train_ets_model(train)
+        forecast_ets = model_ets.forecast(len(valid))
+        forecast_dates = pd.date_range(start=train['data'].iloc[-1] + pd.Timedelta(days=1), periods=len(valid), freq='D')
+        ets_df = pd.DataFrame({'data': forecast_dates, 'previsão': forecast_ets})
+        ets_df = ets_df.merge(valid, on=['data'], how='inner')
+
+        # Calcular métricas de desempenho
+        wmape_ets = wmape(ets_df['realizado'].values, ets_df['previsão'].values)
+        MAE_ets = mean_absolute_error(ets_df['realizado'].values, ets_df['previsão'].values)
+        MSE_ets = mean_squared_error(ets_df['realizado'].values, ets_df['previsão'].values)
+        R2_ets = r2_score(ets_df['realizado'].values, ets_df['previsão'].values)
     
     return ets_df, wmape_ets, MAE_ets, MSE_ets, R2_ets
 
